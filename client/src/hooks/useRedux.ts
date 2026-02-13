fe445696c615d74b0a90a2c630415df06ae40801
Specxyy <shamnadthayyil8@gmail.com>
2026-02-13 12:51:42 +0530

    feat(chat): implement real-time presence, emoji picker, and UI refinements

--- client/src/hooks/useRedux.ts
+++ client/src/hooks/useRedux.ts
@@ -1,9 +1,9 @@
 import { useDispatch, useSelector } from 'react-redux'
+import type { TypedUseSelectorHook } from 'react-redux'
 import type { RootState, AppDispatch } from '../store/store'
 
 export const useAppDispatch = () => useDispatch<AppDispatch>()
-export const useAppSelector = <TSelected>(selector: (state: RootState) => TSelected) =>
-  useSelector(selector)
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
 
 export const useRedux = () => {
   return {
